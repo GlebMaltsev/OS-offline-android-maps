80e31768414b04c7089aa6d6075b630f82c93d30
Gleb.Maltsev <Gleb.Maltsev@effective-soft.com>
2015-11-12 17:10:28 +0300

    add OS offline maps

--- app/src/main/java/com/osofflinemap/MainActivity.java
+++ app/src/main/java/com/osofflinemap/MainActivity.java
@@ -1,37 +1,59 @@
+
 package com.osofflinemap;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.util.ArrayList;
+
+import uk.co.ordnancesurvey.android.maps.BitmapDescriptor;
+import uk.co.ordnancesurvey.android.maps.BitmapDescriptorFactory;
+import uk.co.ordnancesurvey.android.maps.GridPoint;
+import uk.co.ordnancesurvey.android.maps.MapFragment;
+import uk.co.ordnancesurvey.android.maps.MarkerOptions;
+import uk.co.ordnancesurvey.android.maps.OSMap;
+import uk.co.ordnancesurvey.android.maps.OSTileSource;
+
+public class MainActivity extends AppCompatActivity implements OSMap.OnMapClickListener {
+
+    private final static String TAG = MainActivity.class.getSimpleName();
+
+    private OSMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment = ((MapFragment) getFragmentManager().findFragmentById(R.id.map_fragment));
+        mMap = mapFragment.getMap();
+
+        // set list of tileSources
+        ArrayList<OSTileSource> sources = new ArrayList<>();
+        sources.addAll(mMap.localTileSourcesInDirectory(this, getLocalMapTilesDirectory()));
+        mMap.setTileSources(sources);
+
+        // register as OnMapClickListener
+        mMap.setOnMapClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    public boolean onMapClick(GridPoint gp) {
+        final String locationMessage = String.format("Map tapped at OS GridPoint\n{%.0f, %.0f}", gp.x, gp.y);
+        Log.v(TAG, locationMessage);
+        BitmapDescriptor icon = BitmapDescriptorFactory.defaultMarker();
+        mMap.addMarker(new MarkerOptions()
+                .gridPoint(gp)
+                .title("Map clicked here!")
+                .snippet(locationMessage)
+                .icon(icon));
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public static File getLocalMapTilesDirectory() {
+        return new File(Environment.getExternalStorageDirectory().getPath() + "/spargonet/mapping");
     }
 }
